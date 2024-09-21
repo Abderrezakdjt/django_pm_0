089d96ddb0c057582683c1e107f0f71e507ccf26
Abderrezakdja <djabdrzak@gmail.com>
2024-09-21 10:44:19 +0100

    Project Management

--- projects/forms.py
+++ projects/forms.py
@@ -6,7 +6,18 @@ class ProjectCreateForm(forms.ModelForm):
         model = models.Project
         fields = ['category', 'title', 'description']
         widgets = {
-            'category': forms.Select,
-            'title': forms.TextInput,
-            'description': forms.Textarea
+            'category': forms.Select(),
+            'title': forms.TextInput(),
+            'description': forms.Textarea()
+        }
+
+
+class ProjectUpdateForm(forms.ModelForm):
+    class Meta:
+        model = models.Project
+        fields = ['category', 'title', 'status']
+        widgets = {
+            'category': forms.Select(),
+            'title': forms.TextInput(),
+            'status': forms.Select()
         }
